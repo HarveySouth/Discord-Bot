f15fc31648c51145910bd773bcd97165f1e03ae4
z46753hs <harvey.south@student.manchester.ac.uk>
2020-12-19 20:47:56 -0500

    Implment functionality for the channel archive, Create stub to convert data to csv

--- src/databaseInteraction.js
+++ src/databaseInteraction.js
@@ -1,13 +1,68 @@
 require('dotenv').config();
 var assert = require('assert');
 
+var MongoClient = require('mongodb').MongoClient
 
-const MongoClient = require('mongodb').MongoClient;
 var url = process.env.MONGO_URL;
 var database = process.env.MONGO_DB_NAME;
 
-MongoClient.connect(url, function(err, database) {
-  assert.equal(null, err);
-  console.log("Connected successfully to the mongoDB Server");
-  database.close();
-});
+async function dbInsert(collection, query) {
+  MongoClient.connect(url, { useNewUrlParser:true, useUnifiedTopology:true },
+                            function(err, client) {
+    const col = client.db(database).collection(collection);
+    col.insertOne(query, function(err, result) {
+      assert.equal(null, err);
+      client.close();
+    });
+  });
+}
+
+async function dbInsertIfNotExists(collection, query) {
+  MongoClient.connect(url, { useNewUrlParser:true, useUnifiedTopology:true },
+                            function(err, client) {
+    const col = client.db(database).collection(collection);
+    if(!("_id" in query)){
+      col.insertOne(query, function(err, result) {
+        assert.equal(null, err);
+        client.close();
+      });
+    }
+    else{
+      const updateQuery = {_id:query.id};
+      delete query.id;
+      const update = { $set: query};
+      col.updateOne(query, update, { upsert: true }, function(err, result) {
+        assert.equal(null, err);
+        client.close();
+      });
+
+    }
+  });
+}
+
+async function dbInsertUserAndPushGuildIfNotExists(collection, query, pushData) {
+  MongoClient.connect(url, { useNewUrlParser:true, useUnifiedTopology:true },
+                            function(err, client) {
+    const col = client.db(database).collection(collection);
+    if(!("_id" in query)){
+      col.insertOne(query, function(err, result) {
+        assert.equal(null, err);
+        client.close();
+      });
+    }
+    else{
+      const updateQuery = {_id:query.id};
+      delete query.id;
+      const update = { $set: query};
+      col.updateOne(query, update,{ $push: pushData }, { upsert: true }, function(err, result) {
+        assert.equal(null, err);
+        client.close();
+      });
+
+    }
+  });
+}
+
+module.exports ={
+        dbInsertIfNotExists,dbInsert,dbInsertUserAndPushGuildIfNotExists
+    }
--- src/main.js
+++ src/main.js
@@ -14,44 +14,52 @@ client.on('ready', () =>{
     connectedDiscordServer.channels.cache.forEach((channelInServer) => {
       console.log("Channel found: " + channelInServer.name);
       if(channelInServer.isText()){
-        // if(channelInServer.name == "pictures"){
-        //   archive(channelInServer);
-        // }
+        //archive(channelInServer); //uncomment here to archive all connected channels
       }
     });
   });
 });
 
 async function archive(channel) {
-
-    const returnArray = [];
-    let finalMessageID;
-
-    do{
-        const options = { limit: 100 };
-        if (finalMessageID) {
-            options.before = finalMessageID;
+  //put guild and channel in db if does not exists
+  //guild
+  guildID = channel.guild.id;
+  data = {_id:channel.guild.id , name:channel.guild.name};
+  db.dbInsertIfNotExists("server",data);
+  //channel
+  channelID = channel.id;
+  data = {_id:channel.id, name:channel.name, server_id:channel.guild.id};
+  db.dbInsertIfNotExists("textChannel",data);
+  //loop through all messages inserting users and messages
+  let finalMessageID;
+  var foundUsers = [];
+  do{
+      const options = { limit: 100 };
+      if (finalMessageID) {
+          options.before = finalMessageID;
+      }
+      var messageCollection = await channel.messages.fetch(options);
+      messageCollection.forEach((messageInServer) => {
+        if(!(foundUsers.includes(messageInServer.author.id))){
+            var nickname = messageInServer.author.username +"#"+messageInServer.author.discriminator;
+            data = {_id:messageInServer.author.id, nickname:nickname};
+            dataPush = {server_id:guildID}
+            db.dbInsertUserAndPushGuildIfNotExists("user",data,dataPush);
+            foundUsers.push(messageInServer.author.id);
         }
-        var messageCollection = await channel.messages.fetch(options);
-        messageCollection.forEach((messageInServer) => {
-            attachmentMessage = " "
-            // if(messageInServer.attachments.first() != null){
-            //   var attachmentMessage =" " + messageInServer.attachments.first().url;
-            // }
-            //
-            // var convertToReadable = "" + messageInServer.cleanContent + attachmentMessage
-            // returnArray.push(convertToReadable)
-        });
-        if(messageCollection.last()){
-          finalMessageID = messageCollection.last().id;
-        }
-        console.log("Found " + messageCollection.size + " New Messages.");
-    }while(messageCollection.size == 100)
 
-    // returnArray.forEach((messageInServer) => {//prints all messages that were converted to readable
-    //   console.log(messageInServer)
-    // });
+        var date = new Date(messageInServer.createdTimestamp);
+        attachmentMessage = "";
+        if(messageInServer.attachments.first() != null){
+          var attachmentMessage =" " + messageInServer.attachments.first().url;
+        }
+        messageContentFormatted = messageInServer.cleanContent + attachmentMessage;
+        data = {_id:messageInServer.id, content:messageContentFormatted,authorID:messageInServer.author.id,channelID:channelID,date:date};
+        db.dbInsertIfNotExists("chatLog",data);
+      });
+      if(messageCollection.last()){
+        finalMessageID = messageCollection.last().id;
+      }
+      console.log("Found " + messageCollection.size + " New Messages in "+channel.name+".");
+  }while(messageCollection.size == 100)
 }
-
-
-//const dbScript = require('./databaseInteraction');
